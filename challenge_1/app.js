ee612e4e3acb9ed2e6d8c1372d37e2d87bd6d86f
Hieu Ho <hihho@ucdavis.edu>
2020-04-24 13:29:36 -0700

    Added style, revert to tables

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -10,43 +10,69 @@ let boardPiece = function() {
   return piece;
 }
 
-let canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+// let canvas = document.getElementById('canvas');
+// var context = canvas.getContext('2d');
 
-canvas.width = 300;
-canvas.height = 300;
-context.lineWidth = 3;
+// canvas.width = 300;
+// canvas.height = 300;
+// context.lineWidth = 3;
 
-context.beginPath()
-context.moveTo(100, 0);
-context.lineTo(100, 300);
-context.stroke();
+// context.beginPath()
+// context.moveTo(100, 0);
+// context.lineTo(100, 300);
+// context.stroke();
 
-context.beginPath()
-context.moveTo(200, 0);
-context.lineTo(200, 300);
-context.stroke();
+// context.beginPath()
+// context.moveTo(200, 0);
+// context.lineTo(200, 300);
+// context.stroke();
 
-context.beginPath()
-context.moveTo(0, 100);
-context.lineTo(300, 100);
-context.stroke();
+// context.beginPath()
+// context.moveTo(0, 100);
+// context.lineTo(300, 100);
+// context.stroke();
 
-context.beginPath()
-context.moveTo(0, 200);
-context.lineTo(300, 200);
-context.stroke();
-
-
-window.onclick = function(e) {
-  if (e.pageX < 300 && e.pageY < 300) {
-    let clickX = Math.floor(e.pageX / (300/3));
-    let clickY = Math.floor(e.pageY / (300/3));
-    console.log(clickX, clickY)
-  }
-}
+// context.beginPath()
+// context.moveTo(0, 200);
+// context.lineTo(300, 200);
+// context.stroke();
 
 
-document.getElementsByClassName('boardpiece')[0].addEventListener('click', function() {
-  document.getElementsByClassName('boardpiece')[0].innerHTML = boardPiece();
+// window.onclick = function(e) {
+//   if (e.pageX < 300 && e.pageY < 300) {
+//     let clickX = Math.floor(e.pageX / (300/3));
+//     let clickY = Math.floor(e.pageY / (300/3));
+//     console.log(clickX, clickY)
+//     context.font = '30px Arial'
+//     context.fillText('boardPiece()', e.pageX, e.pageY)
+//   }
+// }
+
+
+document.getElementsByClassName('A1')[0].addEventListener('click', function() {
+  document.getElementsByClassName('A1')[0].innerHTML = boardPiece();
+})
+document.getElementsByClassName('A2')[0].addEventListener('click', function() {
+  document.getElementsByClassName('A2')[0].innerHTML = boardPiece();
+})
+document.getElementsByClassName('A3')[0].addEventListener('click', function() {
+  document.getElementsByClassName('A3')[0].innerHTML = boardPiece();
+})
+document.getElementsByClassName('B1')[0].addEventListener('click', function() {
+  document.getElementsByClassName('B1')[0].innerHTML = boardPiece();
+})
+document.getElementsByClassName('B2')[0].addEventListener('click', function() {
+  document.getElementsByClassName('B2')[0].innerHTML = boardPiece();
+})
+document.getElementsByClassName('B3')[0].addEventListener('click', function() {
+  document.getElementsByClassName('B3')[0].innerHTML = boardPiece();
+})
+document.getElementsByClassName('C1')[0].addEventListener('click', function() {
+  document.getElementsByClassName('C1')[0].innerHTML = boardPiece();
+})
+document.getElementsByClassName('C2')[0].addEventListener('click', function() {
+  document.getElementsByClassName('C2')[0].innerHTML = boardPiece();
+})
+document.getElementsByClassName('C3')[0].addEventListener('click', function() {
+  document.getElementsByClassName('C3')[0].innerHTML = boardPiece();
 })
