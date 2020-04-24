0dee306e7e196df34ca390e0025ee1748a529894
Hieu Ho <hihho@ucdavis.edu>
2020-04-24 10:55:29 -0700

    Got toggle piece

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,5 +1,15 @@
 console.log('hello world')
 
+let start = 1;
+let boardPiece = function() {
+  let piece = 'X';
+  if (start % 2 === 0) {
+    piece = 'O';
+  }
+  start++;
+  return piece;
+}
+
 document.getElementsByClassName('boardpiece')[0].addEventListener('click', function() {
-  alert('Hellow');
+  document.getElementsByClassName('boardpiece')[0].innerHTML = boardPiece();
 })
