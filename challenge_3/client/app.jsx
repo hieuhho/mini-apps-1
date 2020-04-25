87eeb4a1d130dbdc647254ebccea38fe4c96faae
Hieu Ho <hihho@ucdavis.edu>
2020-04-25 12:10:39 -0700

    Added form 2

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -9,8 +9,8 @@ class App extends React.Component{
         password: ''
       },
       formTwo: {
-        line1: '',
-        line2: '',
+        addressOne: '',
+        addressTwo: '',
         city: '',
         state: '',
         zip: '',
@@ -44,9 +44,23 @@ class App extends React.Component{
       formOne: formOne
     })
   };
+
   handleOneSubmit(event) {
     event.preventDefault();
     console.log(this.state.formOne);
+  };
+
+  handleChangeTwo(propertyName, event) {
+    const formTwo = this.state.formTwo;
+    formTwo[propertyName] = event.target.value;
+    this.setState({
+      formTwo: formTwo
+    })
+  };
+
+  handleTwoSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.formTwo);
   }
 
   render() {
@@ -74,7 +88,7 @@ class App extends React.Component{
               <label className="password">
                 <input
                 placeholder="Password"
-                type="text"
+                type="password"
                 name="password"
                 value={this.state.formOne.password}
                 onChange={this.handleChangeOne.bind(this, 'password')}/></label>
@@ -84,6 +98,66 @@ class App extends React.Component{
           </form>
 
         </div>
+
+        <div className="formTWo">
+
+          <form>
+
+          <label className="addressOne">
+              <input
+              placeholder="Address"
+              type="text"
+              name="addressOne"
+              value={this.state.formTwo.addressOne}
+              onChange={this.handleChangeTwo.bind(this, 'addressOne')}/></label>
+
+          <label className="addressTWo">
+              <input
+              placeholder="Address 2 (optional)"
+              type="text"
+              name="addressTWo"
+              value={this.state.formTwo.addressTWo}
+              onChange={this.handleChangeTwo.bind(this, 'addressTWo')}/></label>
+
+          <label className="city">
+              <input
+              placeholder="City"
+              type="text"
+              name="city"
+              value={this.state.formTwo.city}
+              onChange={this.handleChangeTwo.bind(this, 'city')}/></label>
+
+          <label className="state">
+              <input
+              placeholder="State"
+              type="text"
+              name="state"
+              value={this.state.formTwo.state}
+              onChange={this.handleChangeTwo.bind(this, 'state')}/></label>
+
+          <label className="zip">
+              <input
+              placeholder="Zip"
+              type="text"
+              name="zip"
+              value={this.state.formTwo.zip}
+              onChange={this.handleChangeTwo.bind(this, 'zip')}/></label>
+
+          <label className="phone">
+              <input
+              placeholder="Phone"
+              type="text"
+              name="phone"
+              value={this.state.formTwo.phone}
+              onChange={this.handleChangeTwo.bind(this, 'phone')}/></label>
+
+          <button className="formTwoSubmit" onClick={this.handleTwoSubmit.bind(this)}>Next</button>
+
+          </form>
+
+        </div>
+
+
         <button>clickme</button>
       </div>
     )
