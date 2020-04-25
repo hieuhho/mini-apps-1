1022cdbe2cbff963c2975b865f1c04c9d6fa7376
Hieu Ho <hihho@ucdavis.edu>
2020-04-25 11:54:20 -0700

    Added form one

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -37,9 +37,53 @@ class App extends React.Component{
   redirect to home
   */
 
+  handleChangeOne(propertyName, event) {
+    const formOne = this.state.formOne;
+    formOne[propertyName] = event.target.value;
+    this.setState({
+      formOne: formOne
+    })
+  };
+  handleOneSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.formOne);
+  }
+
   render() {
     return (
       <div>
+        <div className="formOne">
+          <form>
+
+            <label className="name">
+              <input
+              placeholder="Name"
+              type="text"
+              name="name"
+              value={this.state.formOne.name}
+              onChange={this.handleChangeOne.bind(this, 'name')}/></label>
+
+              <label className="email">
+                <input
+                placeholder="Email"
+                type="text"
+                name="email"
+                value={this.state.formOne.email}
+                onChange={this.handleChangeOne.bind(this, 'email')}/></label>
+
+              <label className="password">
+                <input
+                placeholder="Password"
+                type="text"
+                name="password"
+                value={this.state.formOne.password}
+                onChange={this.handleChangeOne.bind(this, 'password')}/></label>
+
+                <button className="formOneSubmit" onClick={this.handleOneSubmit.bind(this)}>Next</button>
+
+          </form>
+
+        </div>
         <button>clickme</button>
       </div>
     )
