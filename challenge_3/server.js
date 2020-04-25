5d24138b5b3deb85acd622b0920d3cfab4b7e735
Hieu Ho <hihho@ucdavis.edu>
2020-04-25 10:25:55 -0700

    Started server with babel express and nodemon

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express= require('express');
+const app = express();
+const port = 2020;
 
+app.get('/checkout', (req, res) => {
+  res.send('hello World')
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`)
+});
 
